86332f1a73becc5d28435721c2fe1e64e9007fb2
ninja03 <69106571+ninja03@users.noreply.github.com>
2023-03-12 00:27:58 +0900

    a

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { serve } from "https://deno.land/std@0.178.0/http/server.ts";
-export { serveDir } from "https://deno.land/std@0.178.0/http/file_server.ts";
+export { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+export { serveDir } from "https://deno.land/std@0.177.0/http/file_server.ts";
